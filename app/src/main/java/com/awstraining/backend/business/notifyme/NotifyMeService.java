0dc850225b7c29065c7d9e0e91f9ef2c130073e7
Adrian Chomicz <adrian.a.chomicz@capgemini.com>
2024-06-06 13:46:30 +0200

    Add sns service

--- app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
@@ -1,23 +1,29 @@
 package com.awstraining.backend.business.notifyme;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotifyMeService {
 
+    private MessageSender messageSender;
     // TODO: lab1
     //  1. Inject MessageSender.
     // TODO lab2
     //  1. Inject Translator
     // TODO lab3
     //  1. Inject sentiment detector
-//    @Autowired
-    public NotifyMeService() {
+    @Autowired
+    public NotifyMeService(MessageSender messageSender) {
+        this.messageSender = messageSender;
 
     }
     
     public String notifyMe(NotifyMeDO notifyMe) {
-      
+
+        String text = notifyMe.text();
+        messageSender.send(text);
+
         // TODO: lab1
         //  1. Send text using sender.
         //  2. Return sent message.
@@ -27,7 +33,7 @@ public class NotifyMeService {
         // TODO: lab3
         //  1. Detect sentiment of translated message.
         //  2. Change sending of text to "setiment: translated text" and return it.
-        return "This service is not available.";
+        return text;
     }
     
 }
